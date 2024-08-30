2431fbcbb7c9553124e4233d67311f6780ab0315
Nathan Doan <78878767+wolfcutie-alt@users.noreply.github.com>
2024-08-30 13:22:25 +1200

    Fixed

--- BowlingGame.py
+++ BowlingGame.py
@@ -1,34 +1,36 @@
 class BowlingGame:
     def __init__(self):
-        self.rolls=[]
+        self.rolls = []
 
-    def roll(self,pins):
+    def roll(self, pins):
         self.rolls.append(pins)
 
     def score(self):
         result = 0
-        rollIndex=0
+        rollIndex = 0
         for frameIndex in range(10):
-            if frameIndex in range(10):
+            if self.isStrike(rollIndex):
                 result += self.StrikeScore(rollIndex)
-                rollIndex +=1
+                rollIndex += 1
             elif self.isSpare(rollIndex):
                 result += self.spareScore(rollIndex)
-                rollIndex +=2
+                rollIndex += 2
             else:
                 result += self.frameScore(rollIndex)
-            rollIndex +=2
-            return result
+                rollIndex += 2
+        return result
 
     def isStrike(self, rollIndex):
         return self.rolls[rollIndex] == 10
-    def isSpare(self, rollIndex):
-        return self.rolls[rollIndex]+ self.rolls[rollIndex+1]==10
-    def stickeScore(self,rollIndex):
-        return  10+ self.rolls[rollIndex+1]+ self.rolls[rollIndex+2]
 
-    def spareScore(self,rollIndex):
-        return  10+ self.rolls[rollIndex+2]
+    def isSpare(self, rollIndex):
+        return self.rolls[rollIndex] + self.rolls[rollIndex + 1] == 10
+
+    def StrikeScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 1] + self.rolls[rollIndex + 2]
+
+    def spareScore(self, rollIndex):
+        return 10 + self.rolls[rollIndex + 2]
 
     def frameScore(self, rollIndex):
         return self.rolls[rollIndex] + self.rolls[rollIndex + 1]
--- test_game.py
+++ test_game.py
@@ -8,47 +8,55 @@ class TestBowlingGame(unittest.TestCase):
 
     def testGutterGame(self):
         for i in range(0, 20):
-            self.game.rolls(0)
-        assert self.game.score()==0
+            self.game.roll(0)
+        self.assertEqual(self.game.score(), 0)
+
     def testAllOnes(self):
         self.rollMany(1, 20)
-        assert self.game.score()==20
+        self.assertEqual(self.game.score(), 20)
+
     def testOneSpare(self):
-        self.game.rolls(5)
-        self.game.rolls(5)
-        self.game.rolls(3)
-        self.rollMany(0,17)
-        assert self.game.score()==16
+        self.game.roll(5)
+        self.game.roll(5)  
+        self.game.roll(3)
+        self.rollMany(0, 17)
+        self.assertEqual(self.game.score(), 16)
+
     def testOneStrike(self):
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(3)
-        self.rollMany(0,16)
-        assert  self.game.score()==24
+        self.game.roll(10)  
+        self.game.roll(4)
+        self.game.roll(3)
+        self.rollMany(0, 16)
+        self.assertEqual(self.game.score(), 24)
+
     def testPerfectGame(self):
-        self.rollMany(10,12)
-        assert self.game.score()==300
-    def testOneSpare(self):
-        self.rollMany(5,21)
-        assert self.game.score()==150
+        self.rollMany(10, 12)
+        self.assertEqual(self.game.score(), 300)
+
+    def testAllFives(self):
+        self.rollMany(5, 21)
+        self.assertEqual(self.game.score(), 150)
+
     def testTwoSpare(self):
-        self.game.rolls(2)
-        self.game.rolls(8)
-        self.game.rolls(3)
-        self.game.rolls(7)
-        self.game.rolls(4)
+        self.game.roll(2)
+        self.game.roll(8)  
+        self.game.roll(3)
+        self.game.roll(7)  
+        self.game.roll(4)
         self.rollMany(0, 15)
-        assert self.game.score() == 31
+        self.assertEqual(self.game.score(), 31)
+
     def testTwoStrike(self):
-        self.game.rolls(10)
-        self.game.rolls(10)
-        self.game.rolls(4)
-        self.game.rolls(2)
+        self.game.roll(10)  
+        self.game.roll(10)  
+        self.game.roll(4)
+        self.game.roll(2)
         self.rollMany(0, 14)
-        assert self.game.score() == 46
-    def rollMany(self, pins,rolls):
+        self.assertEqual(self.game.score(), 46)
+
+    def rollMany(self, pins, rolls):
         for i in range(rolls):
-            self.game.rolls(pins)
-            
-    if __name__ == '__main__':
-        unittest.main()
+            self.game.roll(pins)
+
+if __name__ == '__main__':
+    unittest.main()
